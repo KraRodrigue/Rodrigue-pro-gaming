aed3688f2d649d4c9d54591da4c3bef97e17c066
N'GUETTA  KRA VIVIEN RODRIGUE <169077191+KraRodrigue@users.noreply.github.com>
2025-02-06 18:28:31 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+    
+    <div class="footer-nav">
+      <div class="footer-logo">
+        <img src="./Images/logo 2.png" alt="">
+        <h5>Rodrigue pro gaming </h5>
+        <p>Contactez-moi : <a href="mailto:rodrigueprogaming@email.com">rodrigueprogaming@email.com</a></p>
+      </div>
+      <div class="link-link">
+        <div class="footer-links">
+          <a href="#">Accueil</a>
+          <a href="#">Mobile</a>
+          <a href="#">psp</a>
+          <a href="#">ps2</a>
+        </div>
+        <div class="footer-links1">
+          <a href="#"> ps vita</a>
+          <a href="#"> wii</a>
+          <a href="#"> Nintendo Switch</a>
+          <a href="#"> Pc</a>
+        </div>
+      </div>  
+    </div>
+    <div class="footer-content">
+      <p>© 2025 Rodrigue Pro Gaming - Tous droits réservés</p>
+      <p>Jouez à vos jeux favoris sur Android grâce à nos guides et astuces !</p>
+    </div>
+    <div class="link-icone">
+      <a href="https://www.youtube.com/@RodrigueproGaming/videos"><i class="fa-brands fa-youtube"></i></a>
+      <a href="https://www.tiktok.com/@madarapro.game?_t=ZN-8soQ61l9DaY&_r=1"><i class="fa-brands fa-tiktok"></i></a>
+      <a href="https://t.me/+VLZnqGYWPDdhOTk0"><i class="fa-brands fa-telegram"></i></a> 
+      <a href=""><i class="fa-brands fa-instagram"></i></a>
+      <a href=""><i class="fa-brands fa-facebook"></i></a>
+    </div>
+
+  </footer> 
